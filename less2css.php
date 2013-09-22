828f4611c772030a7d468195cff09301f966070e
Jesus Christian Cruz Acono <compermisos@gmail.com>
2013-09-22 02:26:19 -0500

    added changes to make minify

--- less2css.php
+++ less2css.php
@@ -83,9 +83,35 @@ function deTree($tree, &$out = array()){
 }
 
 
-
-function genCSS($lessDir = 'less/', $cssDir = 'css/', $lessExt = 'less' ){
+function compileFile($fname, $outFname = null) {
+	if (!is_readable($fname)) {
+		throw new Exception('load error: failed to find '.$fname);
+	}
+	
+	$pi = pathinfo($fname);
+	
 	require "lessc.inc.php";
+	require "CssMin.php";
+	$less = new lessc;
+	
+	$out = $result = CssMin::minify($less->compile(file_get_contents($fname), $fname));
+	
+	if ($outFname !== null) {
+		return file_put_contents($outFname, $out);
+	}
+	return $out;
+}
+
+// compile only if changed input has changed or output doesn't exist
+function checkedCompile($in, $out) {
+	if (!is_file($out) || filemtime($in) > filemtime($out)) {
+		compileFile($in, $out);
+		return true;
+	}
+	return false;
+}
+function genCSS($lessDir = 'less/', $cssDir = 'css/', $lessExt = 'less' ){
+	#require "lessc.inc.php";
 	$unclean = 1;
 	$tree = parser($lessDir, $lessExt, 10, $lessDir);
 	$cleanTree = array();
@@ -98,7 +124,7 @@ function genCSS($lessDir = 'less/', $cssDir = 'css/', $lessExt = 'less' ){
 		}
 	}
 	$tree = deTree($tree);
-	$less = new lessc;
+	#$less = new lessc;
 	foreach($tree as $file){
 		$cssName = $cssDir . $file['pathname'] . $file['namenotype'] . '.css';
 		$lessName = $lessDir . $file['pathname'] . $file['namenotype'] . '.' . $lessExt;
@@ -107,7 +133,7 @@ function genCSS($lessDir = 'less/', $cssDir = 'css/', $lessExt = 'less' ){
 			mkdir($cssCDir, 0755, TRUE);
 		}
 		try {
-			$less->checkedCompile($lessName, $cssName);
+			checkedCompile($lessName, $cssName);
 		} catch (exception $e) {
 			echo "fatal error: " . $e->getMessage();
 		}
